4c9b09ebd41034459c5ec5d07f4a2bc00852d555
Phakin <phakin.che@gmail.com>
2017-10-17 21:41:57 +0700

    Add based code

---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask, url_for, send_from_directory, request
+import logging, os
+from werkzeug import secure_filename
+
+app = Flask(__name__)
+file_handler = logging.FileHandler('server.log')
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+
+PROJECT_HOME = os.path.dirname(os.path.realpath(__file__))
+UPLOAD_FOLDER = '{}/uploads/'.format(PROJECT_HOME)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def create_new_folder(local_dir):
+    newpath = local_dir
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    return newpath
+
+@app.route('/', methods = ['POST'])
+def api_root():
+    app.logger.info(PROJECT_HOME)
+    if request.method == 'POST' and request.files['image']:
+    	app.logger.info(app.config['UPLOAD_FOLDER'])
+    	img = request.files['image']
+    	img_name = secure_filename(img.filename)
+    	create_new_folder(app.config['UPLOAD_FOLDER'])
+    	saved_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
+    	app.logger.info("saving {}".format(saved_path))
+    	img.save(saved_path)
+    	return send_from_directory(app.config['UPLOAD_FOLDER'],img_name, as_attachment=True)
+    else:
+    	return "Where is the image?"
+
+if __name__ == '__main__':
+    app.run(debug=True)
